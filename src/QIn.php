e7bf875f1daaddcbe4070606bd0b77b2645612b7
Matt Stone <mstone@mrn.org>
2021-04-28 14:13:37 -0500

    Warn on in [], use false instead

--- src/QIn.php
+++ src/QIn.php
@@ -8,6 +8,11 @@ class QIn extends QCondition
 
     public function __construct(string $column, array $values)
     {
+        if (count($values) === 0) {
+            trigger_error('Array of length 0 passed to QIn constructor, using FALSE instead', E_USER_WARNING);
+            parent::__construct("FALSE", []);
+        }
+
         parent::__construct("$column IN (" . self::bindingsString($values) . ")", $values);
     }
 }
