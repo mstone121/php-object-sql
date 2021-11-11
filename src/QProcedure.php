076e832d171ff6b5bed6de2768f0aab351b0a3fa
Matt Stone <mstone@mrn.org>
2021-11-11 12:14:38 -0600

    Revert "Add types to procedure bindings"
    This reverts commit f90d8229a8f7442e9d3f73630cd4f5337d098172.

--- src/QProcedure.php
+++ src/QProcedure.php
@@ -6,51 +6,20 @@ class QProcedure extends QComponent
 {
     protected $procedure;
     protected $parameters;
-    protected $types;
 
     public function __construct(string $procedure, array $parameters = [])
     {
         $this->procedure = $procedure;
-
-        $this->parameters = [];
-        $this->types = [];
-
-        foreach ($parameters as $parameter) {
-            if (is_array($parameter)) {
-                if (count($parameter) !== 2) {
-                    throw new \InvalidArgumentException('Invalid array parameter type, expecting array of length 2: [value, type]');
-                }
-
-                $this->parameters[] = $parameter[0];
-                $this->types[] = $parameter[1];
-            } else {
-                $this->parameters[] = $parameter;
-                $this->types[] = null;
-            }
-        }
+        $this->parameters = $parameters;
     }
 
     public function __toString(): string
     {
-        return "$this->procedure(" . $this->getBindingsString() . ")";
+        return "$this->procedure(" . implode(',', self::bindingsStringFromMixedArray($this->parameters)) . ")";
     }
 
     public function getBindings(): array
     {
         return self::bindingsFromMixedArray($this->parameters);
-    }
-
-    protected function getBindingsString(): string
-    {
-        $index = 0;
-        $bindings = [];
-        while (isset($this->parameters[$index])) {
-            $type = $this->types[$index];
-
-            $bindings[] = $type ? '?::' . $type : '?';
-            $index++;
-        }
-
-        return implode(',', $bindings);
     }
 }
